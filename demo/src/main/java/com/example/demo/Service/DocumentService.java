ddacdbfd67a76851fe2ae8d08a70d84dfe117182
yenlove1990 <chunyenyu1@gmail.com>
2023-06-29 16:02:54 -0700

    Revert "connecting to mysql database, changed in application.properties"
    This reverts commit b6bf28fede073b61592fbc26d7e800b50b64295d.

--- demo/src/main/java/com/example/demo/Service/DocumentService.java
+++ demo/src/main/java/com/example/demo/Service/DocumentService.java
@@ -2,6 +2,8 @@ package com.example.demo.Service;
 
 import com.example.demo.DAO.DocumentDAO;
 import com.example.demo.Entity.Document;
+import com.example.demo.Service.AlreadyExistingException;
+import com.example.demo.Service.NotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,43 +11,39 @@ import java.util.List;
 
 @Service
 public class DocumentService {
-    @Autowired
     private final DocumentDAO documentDao;
-
-    public DocumentService(DocumentDAO documentDao) {
-        this.documentDao = documentDao;
+    @Autowired
+    public DocumentService(DocumentDAO documentDAO){
+        this.documentDao= documentDAO;
     }
-
-
     public Document getDocument(int id) throws NotExistException {
-        return documentDao.findById(id)
-                .orElseThrow(() -> new NotExistException("Document not found"));
+        Document temp = documentDao.getDocument(id);
+        if(temp==null){
+            throw new NotExistException("Document with "+ id+" not exist");
+        }
+        return temp;
 
     }
     public List<Document> listDocuments() {
-        return documentDao.findAll();
+        return documentDao.listDocuments();
 
     }
     public void create(Document doc) throws AlreadyExistingException {
-        try{
-            documentDao.save(doc);
-        }
-        catch (Exception e){
+        if(documentDao.getDocument(doc.getId()) != null) {
             throw new AlreadyExistingException("A document with id " + doc.getId() + " already exists.");
         }
-
+        documentDao.create(doc);
     }
     public void delete(int id) throws NotExistException{
-        if(!documentDao.existsById(id)){
+        if(documentDao.delete(id)==null){
             throw new NotExistException("Document with id "+ id+" not exist");
-        }
-        documentDao.deleteById(id);
+        };
     }
     public Document update(Document doc)throws NotExistException{
-        if(!documentDao.existsById(doc.getId())){
+        Document temp = documentDao.getDocument(doc.getId());
+        if(temp == null){
             throw new NotExistException("Document with id "+ doc.getId()+" not exist");
         }
-        documentDao.save(doc);
-        return doc;
+        return documentDao.update(doc);
     }
 }
