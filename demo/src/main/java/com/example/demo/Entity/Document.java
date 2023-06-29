64fc9ea9d5ba3d9701dbb3e63a9cf093051da3a5
yenlove1990 <chunyenyu1@gmail.com>
2023-06-29 16:16:00 -0700

    Revert "Revert "connecting to mysql database, changed in application.properties""
    This reverts commit ddacdbfd67a76851fe2ae8d08a70d84dfe117182.

--- demo/src/main/java/com/example/demo/Entity/Document.java
+++ demo/src/main/java/com/example/demo/Entity/Document.java
@@ -1,10 +1,15 @@
 package com.example.demo.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Table(schema = "users")
+@Entity
 public class Document {
-
+    @Id
     private int id;
     private String content;
 
